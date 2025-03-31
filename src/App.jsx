f2bff7b91a9aa8fc9731290a500827f95f8bd098
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-31 16:03:56 +0100

    Add single article path

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import "./newApp.css";
 import Header from "./Header";
 import Nav from "./Nav";
 import Articles from "./Articles";
+import SingleArticle from "./SingleArticle";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-
   return (
     <>
       <Header></Header>
       <Nav></Nav>
-      <Articles></Articles>
+
+      <Routes>
+        <Route path="/" element={<Articles />} />
+        <Route path="/articles/:article_id" element={<SingleArticle />} />
+      </Routes>
     </>
   );
 }
