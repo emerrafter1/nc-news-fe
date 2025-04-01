6b836eea3b20489e817ea01d51339335236c1e16
emerrafter1 <emerlouiserafter@gmail.com>
2025-04-01 13:38:13 +0100

    add Comment adder

---
+++ src/CommentAdder.jsx
@@ -0,0 +1,61 @@
+import { postComment } from "../api";
+import { useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
+
+function CommentAdder({ comments, setComments, article_id }) {
+  const [commentInput, setCommentInput] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false)
+
+  function handleAddComment(event) {
+    setIsLoading(true)
+    setError(null);
+    event.preventDefault();
+    if(commentInput.trim().length < 1) return setError("Comment cannot be empty!")
+    postComment(article_id, commentInput)
+      .then((addedComment) => {
+       
+        setCommentInput("");
+        setComments([addedComment, ...comments]); //comment goes to top
+        
+      })
+      .catch((error) => {
+        console.log(error);
+        setError("Your comment failed to post! Please try again.");
+      }).finally(() => {
+        setIsLoading(false)
+      });
+  }
+
+  if (isLoading) {
+
+    return (
+      
+        <LoadingSpinner loadingMessage={`Adding comment...`}/>
+      
+    );
+  }
+
+
+
+
+  function handleCommentInputChange(event) {
+    setCommentInput(event.target.value);
+  }
+
+  return (
+  
+      <form id="add-comment-form" onSubmit={handleAddComment}>
+        <textarea
+          placeholder="Add a comment"
+          value={commentInput}
+          onChange={handleCommentInputChange}
+        ></textarea>
+        {error ? <p>{error}</p> : null}
+        <button type="submit">Comment</button>
+      </form>
+  
+  );
+}
+
+export default CommentAdder;
