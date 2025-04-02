ba12b6afa017fa7ac5949857e3ecd815cac24b91
emerrafter1 <emerlouiserafter@gmail.com>
2025-04-02 12:06:11 +0200

    update simple styling

--- src/ArticleCard.jsx
+++ src/ArticleCard.jsx
@@ -6,14 +6,22 @@ import { Link } from "react-router-dom";
 function ArticleCard({ article }) {
   return (
     <li className="article-card">
-    <div className="article-info">
-      <p className="author">{article.author}</p>
-      <p><Link to={`/articles/${article.article_id}`}>{article.title}</Link></p>
-      <p>
-        <TimeAgo date={convertToISODate(article.created_at)} />
-      </p>
-      <p className="pill">{article.votes} votes</p>
-      <p className="pill">{article.comment_count} comments</p>
+      <div className="article-info">
+        <div className="card-heading">
+          <p className="topic">{article.topic}</p>
+          <p>•</p>
+          <p>
+            <TimeAgo date={convertToISODate(article.created_at)} />
+          </p>
+        </div>
+        <p className="author">{article.author}</p>
+
+        <Link to={`/articles/${article.article_id}`}>
+          <p>{article.title}</p>
+        </Link>
+
+        <p className="pill">{article.votes} votes</p>
+        <p className="pill">{article.comment_count} comments</p>
       </div>
       <img src={article.article_img_url} alt={article.title} />
     </li>
