580c8428fd4105179b82365382b1ab25e58cd6e2
emerrafter1 <emerlouiserafter@gmail.com>
2025-04-02 12:09:19 +0200

    update user context

--- src/contexts/User.jsx
+++ src/contexts/User.jsx
@@ -4,7 +4,11 @@ import {createContext, useState} from 'react'
 export const UserContext = createContext();
 
 export const UserProvider = ({children}) => {
-    const [loggedInUser, setLoggedInUser] = useState("happyamy2016")
+    const [loggedInUser, setLoggedInUser] = useState({
+        username: "happyamy2016",
+        name: "Amy Happy",
+        avatar_url: "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729"
+        })
 
     return <UserContext.Provider value={{loggedInUser, setLoggedInUser}}>{children}</UserContext.Provider>
 }
