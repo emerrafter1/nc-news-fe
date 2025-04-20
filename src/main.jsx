111565fcc2fa201f747f4acc53e574e9eaded37c
emerrafter1 <emerlouiserafter@gmail.com>
2025-04-20 10:47:43 +0100

    remove unused files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index2.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
